87ac3e2ac15dd5ef794897be718ff94c1359f0b8
Titouan Rigoudy <titouan.rigoudy@gmail.com>
2016-04-10 16:40:15 +0200

    Use string action types for redux-logger.

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -1,12 +1,13 @@
 // Socket actions
-export const SOCKET_SET_OPEN        = Symbol("SOCKET_SET_OPEN");
-export const SOCKET_SET_OPENING     = Symbol("SOCKET_SET_OPENING");
-export const SOCKET_SET_CLOSED      = Symbol("SOCKET_SET_CLOSED");
-export const SOCKET_SET_CLOSING     = Symbol("SOCKET_SET_CLOSING");
-export const SOCKET_SET_ERROR       = Symbol("SOCKET_SET_ERROR");
-export const SOCKET_RECEIVE_MESSAGE = Symbol("SOCKET_RECEIVE_MESSAGE");
-export const SOCKET_SEND_MESSAGE    = Symbol("SOCKET_SEND_MESSAGE");
+export const SOCKET_SET_OPEN        = "SOCKET_SET_OPEN";
+export const SOCKET_SET_OPENING     = "SOCKET_SET_OPENING";
+export const SOCKET_SET_CLOSED      = "SOCKET_SET_CLOSED";
+export const SOCKET_SET_CLOSING     = "SOCKET_SET_CLOSING";
+export const SOCKET_SET_ERROR       = "SOCKET_SET_ERROR";
+export const SOCKET_RECEIVE_MESSAGE = "SOCKET_RECEIVE_MESSAGE";
+export const SOCKET_SEND_MESSAGE    = "SOCKET_SEND_MESSAGE";
 
 // Room actions
-export const ROOM_SELECT = Symbol("ROOM_SELECT");
-export const ROOM_JOIN   = Symbol("ROOM_JOIN");
+export const ROOM_SELECT = "ROOM_SELECT";
+export const ROOM_JOIN   = "ROOM_JOIN";
+export const ROOM_SAY    = "ROOM_SAY";
