106896d1455c23e159f1050dcb55b83db0f5b524
Titouan Rigoudy <titouan.rigoudy@gmail.com>
2016-05-24 15:30:33 +0200

    Make RoomsPane a pure component.

--- src/containers/RoomsPane.js
+++ src/containers/RoomsPane.js
@@ -9,53 +9,47 @@ import RoomChat from "../components/RoomChat";
 import RoomList from "../components/RoomList";
 import RoomUserList from "../components/RoomUserList";
 
-class RoomsPane extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+const RoomsPane = (props) => {
+    const { loginUserName, params, rooms, roomActions } = props;
 
-    render() {
-        const { loginUserName, params, rooms, roomActions } = this.props;
+    let roomName;
+    let roomChat;
 
-        let roomName;
-        let roomChat;
+    if (params && params.roomNameHash) {
+        roomName = rooms.getNameByHash(params.roomNameHash);
 
-        if (params && params.roomNameHash) {
-            roomName = rooms.getNameByHash(params.roomNameHash);
+        const roomData = rooms.getByName(roomName);
 
-            const roomData = rooms.getByName(roomName);
+        if (roomData) {
+            const room = {
+                name: roomName,
+                membership: roomData.get("membership"),
+                messages:   roomData.get("messages"),
+                showUsers:  roomData.get("showUsers")
+            };
 
-            if (roomData) {
-                const room = {
-                    name: roomName,
-                    membership: roomData.get("membership"),
-                    messages:   roomData.get("messages"),
-                    showUsers:  roomData.get("showUsers")
-                };
-
-                roomChat = (
-                    <RoomChat
-                        loginUserName={loginUserName}
-                        room={room}
-                        roomActions={roomActions}
-                    />
-                );
-            }
-        }
-
-        return (
-            <div id="rooms-pane">
-                <RoomList
-                    rooms={rooms}
+            roomChat = (
+                <RoomChat
+                    loginUserName={loginUserName}
+                    room={room}
                     roomActions={roomActions}
                 />
-                <div id="room-selected-pane">
-                    {roomChat}
-                </div>
-            </div>
-        );
+            );
+        }
     }
-}
+
+    return (
+        <div id="rooms-pane">
+            <RoomList
+                rooms={rooms}
+                roomActions={roomActions}
+            />
+            <div id="room-selected-pane">
+                {roomChat}
+            </div>
+        </div>
+    );
+};
 
 RoomsPane.propTypes = {
     loginUserName: PropTypes.string.isRequired,
